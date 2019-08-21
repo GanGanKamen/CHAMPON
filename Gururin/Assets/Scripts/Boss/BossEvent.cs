321384f37a2cb11d516c56af714518a19df67f71
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-08-22 02:38:19 +0900

    ボス登場アニメーションの変更。夜は焼肉っしょ！

--- Gururin/Assets/Scripts/Boss/BossEvent.cs
+++ Gururin/Assets/Scripts/Boss/BossEvent.cs
@@ -14,11 +14,17 @@ public class BossEvent : MonoBehaviour
     private Vector2 topDis, bottomDis;
     private bool stop = true;
 
-    [SerializeField] private GameObject bossChara,boss;
+    [SerializeField] private BossChara bossChara;
+    [SerializeField] private GameObject boss;
     [SerializeField] private GameObject[] wind;
+    [SerializeField] private FanWingRotation[] fans;
+    [SerializeField] private Animator windowAnim;
+
+    private PlayerMove player;
     // Start is called before the first frame update
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
         topBand.localPosition = topStart;
         bottomBand.localPosition = bottomStart;
         topDis = topStart;
@@ -50,19 +56,42 @@ public class BossEvent : MonoBehaviour
             yield return null;
         }
         stop = true;
-        bossChara.SetActive(true);
-        yield return new WaitForSeconds(5f);
-        for(int i = 0; i < wind.Length; i++)
+        bossChara.gameObject.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        Vector2 force = new Vector2(0, 300f);
+        player.GetComponent<Rigidbody2D>().AddForce(force);
+        while(bossChara.hasDown == false)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
+        bossChara.Recovery();
+        for(int i = 0; i < fans.Length; i++)
+        {
+            fans[i].windAct = true;
+        }
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < wind.Length; i++)
         {
             wind[i].SetActive(true);
         }
         yield return new WaitForSeconds(1f);
+        windowAnim.SetBool("Close", false);
+        windowAnim.SetBool("Open", true);
+        yield return new WaitForSeconds(1f);
+        while (!Input.GetMouseButtonDown(0))
+        {
+            yield return null;
+        }
+        
+        windowAnim.SetBool("Open", false);
+        windowAnim.SetBool("Close", true);
         MovieCutOut();
         while (Mathf.Abs(topBand.localPosition.y - topDis.y) > 1f)
         {
             yield return null;
         }
-        bossChara.SetActive(false);
+        bossChara.gameObject.SetActive(false);
         boss.SetActive(true);
         gamecontroller.isCon = false;
     }
