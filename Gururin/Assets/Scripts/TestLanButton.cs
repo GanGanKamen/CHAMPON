5456f973ba569eeb28a91e1026dbc9175ff72cbb
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-08-31 15:52:27 +0900

    中国語翻訳完了。I have translated the janpanese to Chinese

---
+++ Gururin/Assets/Scripts/TestLanButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLanButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LanSwitchText(int lan)
+    {
+        switch (lan)
+        {
+            case 0:
+                LanguageSwitch.language = LanguageSwitch.Language.Japanese;
+                break;
+            case 1:
+                LanguageSwitch.language = LanguageSwitch.Language.English;
+                break;
+            case 2:
+                LanguageSwitch.language = LanguageSwitch.Language.ChineseHans;
+                break;
+            case 3:
+                LanguageSwitch.language = LanguageSwitch.Language.ChineseHant;
+                break;
+        }
+    }
+}
